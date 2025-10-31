b3b8dfa95864f6b4a9649007acf2d027c95f1db5
Mohamed Shokry <m7md015shokry@gmail.com>
2025-10-31 10:02:02 +0200

    feat: Add unit test for Calculator
    Adds a new unit test file, `test/calculator_unit_test.dart`, to verify the functionality of the `Calculator` class.
    
    This initial test case checks the `add` method, asserting that the sum of 2 and 3 correctly returns 5.

---
+++ test/calculator_unit_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test("the add method should be return 5 when the number1=2 & number2=3", () {
+    //arrange
+    final calculator = Calculator();
+    const number1 = 2;
+    const number2 = 3;
+    const expectedResult = 5;
+
+    //act
+    final actualResult = calculator.add(number1, number2);
+
+    //assert
+    expect(actualResult, expectedResult);
+  });
+}
