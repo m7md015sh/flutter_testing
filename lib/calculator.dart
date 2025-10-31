046d4fea5c42c337f0d7d2066a99e4252fe1c57a
Mohamed Shokry <m7md015shokry@gmail.com>
2025-10-31 10:29:51 +0200

    feat: Add zero division check in Calculator
    Adds a check in the `divide` method of the `Calculator` class to prevent division by zero. If the divisor is zero, the method now throws an exception.

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -5,6 +5,7 @@ class Calculator {
   }
 
   int divide(int num1, int num2) {
+    if (num2 == 0) throw Exception("you can't divide by zero");
     final result = num1 ~/ num2;
     return result;
   }
