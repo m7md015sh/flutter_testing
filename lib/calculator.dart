9870f295bec5fe1b9dc2c3d27d04950dc901507f
Mohamed Shokry <m7md015shokry@gmail.com>
2025-10-31 10:13:13 +0200

    refactor: Use intermediate variables for return values
    Introduces a `result` variable in the `add` and `divide` methods of the `Calculator` class to hold the calculated value before returning it.

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -1,9 +1,11 @@
 class Calculator {
   int add(int num1, int num2) {
-    return num1 + num2;
+    final result = num1 + num2;
+    return result;
   }
 
   int divide(int num1, int num2) {
-    return num1 ~/ num2;
+    final result = num1 ~/ num2;
+    return result;
   }
 }
