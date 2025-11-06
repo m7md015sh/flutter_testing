4eb8b57c3278065d18eb8e74a0213273bb035689
Mohamed Shokry <m7md015shokry@gmail.com>
2025-11-06 13:14:40 +0200

    feat: Implement PostCubit and state handling in PostsScreen
    Integrates `PostCubit` into the `PostsScreen` to manage and display the state of fetching posts.
    
    This change involves:
    - Wrapping the `Scaffold` with a `BlocProvider` to create and provide `PostCubit`.
    - Initializing the `PostCubit` with its dependencies (`PostRepositoryImpl`, `RemoteDataSourceImpl`, `NetworkServiceImpl`) and calling `getPosts()`.
    - Using `BlocBuilder` to listen to `PostState` changes and update the UI accordingly.
    - Displaying a `CircularProgressIndicator` for the `PostLoadingState`.
    - Showing a `ListView.builder` to list posts for the `PostLoadedState`, with each `ListTile` displaying the post's id, title, body, and userId.
    - Displaying an error message for the `PostErrorState`.
    - Adding an `AppBar` with the title "Posts".

--- lib/presentation/posts_screen.dart
+++ lib/presentation/posts_screen.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_testing/business_logic/post_cubit/post_cubit.dart';
+import 'package:flutter_testing/business_logic/post_cubit/post_state.dart';
+import 'package:flutter_testing/core/services/network_service.dart';
+import 'package:flutter_testing/data/repository/post_repository.dart';
+import 'package:flutter_testing/data_source/remote_data_source.dart';
 
 class PostsScreen extends StatelessWidget {
   const PostsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Posts Screen')));
+    return BlocProvider(
+      create: (context) => PostCubit(
+        PostRepositoryImpl(RemoteDataSourceImpl(NetworkServiceImpl())),
+      )..getPosts(),
+      child: Scaffold(
+        appBar: AppBar(title: const Text('Posts')),
+        body: SafeArea(
+          child: BlocBuilder<PostCubit, PostState>(
+            builder: (context, state) {
+              if (state is PostLoadingState) {
+                return Center(child: CircularProgressIndicator());
+              } else if (state is PostLoadedState) {
+                return ListView.builder(
+                  itemCount: state.posts.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      leading: Text(state.posts[index].id.toString()),
+                      trailing: Text(state.posts[index].userId.toString()),
+                      title: Text(state.posts[index].title),
+                      subtitle: Text(state.posts[index].body),
+                    );
+                  },
+                );
+              } else if (state is PostErrorState) {
+                return SizedBox(child: Center(child: Text(state.message)));
+              } else {
+                return SizedBox();
+              }
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
